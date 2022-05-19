9dc4d81bbb41cb6e4552e82458f801290262c9f2
Prakash-Dhankecha <80136422+Prakash-Dhankecha@users.noreply.github.com>
2022-05-19 18:42:42 +0530

    Delete 20SOECE11028_Demo PSEE directory

--- 20SOECE11028_Demo PSEE/savedata.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("include/connection.php");
-    $gallery_name=isset($_POST['gallery_name'])?$_POST['gallery_name']:"";
-    $gallery_images=isset($_POST['gallery_images'])?$_POST['gallery_images']:"";
-    $sql = "insert into gd_gallery_sub(gallery_name,gallery_images)values('$gallery_name','$gallery_images')";
-    $db->query($sql);
-    header("location:index.php");
-?>
